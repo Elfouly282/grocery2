0f3581279e9f2a86cae18b99a9d933611dc28541
Mahmoud Yasser <mahmoudyaser20196@gmail.com>
2026-04-17 14:17:03 +0200

    finish cubit

---
+++ lib/feature/login/presentation/cubit/login_cubit.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../data/repo/repo.dart';
+import 'login_state.dart';
+
+class AuthCubit extends Cubit<AuthStates> {
+  final AuthRepo authRepo;
+
+  AuthCubit(this.authRepo) : super(AuthInitialState());
+
+  Future<void> login({required String email, required String password}) async {
+    emit(AuthLoadingState());
+
+    try {
+      final user = await authRepo.login(
+        email: email,
+        password: password,
+      );
+
+      if (user != null) {
+        emit(AuthSuccessState(user));
+      } else {
+        emit(AuthErrorState("UnExpected Error Please, try again"));
+      }
+    } catch (e) {
+      emit(AuthErrorState(e.toString()));
+    }
+  }
+}
